3c579bd7bba5e143f5eb96bd06d1f225e41a0d47
Giulio De Donato <liuggio@gmail.com>
2015-09-23 19:19:54 +0200

    removed unused const

--- src/Event/EventsName.php
+++ src/Event/EventsName.php
@@ -4,13 +4,9 @@ namespace Liuggio\Fastest\Event;
 
 final class EventsName
 {
-    const CHANNEL_MANAGER_STARTED = 'channel_manager_completed';
-    const CHANNEL_IS_WAITING_A_COMMAND_LINE = 'channel_is_waiting_a_command_line';
-    const CHANNEL_IS_WAITING_A_BEFORE_COMMAND_LINE = 'channel_is_waiting_a_before_command_line';
     const PROCESS_STARTED = 'process_started';
     const PROCESS_COMPLETED = 'process_completed';
     const PROCESS_GENERATED_BUFFER = 'process_generated_buffer';
-
     const INPUT_LINE_ENQUEUED = 'command_line_enqueued';
     const INPUT_LINE_DEQUEUED = 'command_line_dequeued';
     const LOOP_STARTED = 'loop_started';
