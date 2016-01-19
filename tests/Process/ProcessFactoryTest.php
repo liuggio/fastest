0b2ed423b3ea3810a306686ec5052b65cad7d7b9
loostro <loostro@gmail.com>
2016-01-19 17:43:27 +0100

    Update ProcessFactoryTest.php
    Update tests to include ENV variables

--- tests/Process/ProcessFactoryTest.php
+++ tests/Process/ProcessFactoryTest.php
@@ -15,14 +15,17 @@ class ProcessFactoryTest extends \PHPUnit_Framework_TestCase
         $process = $factory->createAProcess('fileA', 2, 10, true);
 
         $this->assertEquals('bin/phpunit fileA', $process->getCommandLine());
-        $this->assertEquals(array(
-            0 => 'ENV_TEST_CHANNEL=2',
-            1 => 'ENV_TEST_CHANNEL_READABLE=test_2',
-            2 => 'ENV_TEST_CHANNELS_NUMBER=10',
-            3 => 'ENV_TEST_ARGUMENT=fileA',
-            4 => 'ENV_TEST_INC_NUMBER=10',
-            5 => 'ENV_TEST_IS_FIRST_ON_CHANNEL=1'),
-            $process->getenv());
+        $this->assertEquals(
+            $_ENV + array(
+                0 => 'ENV_TEST_CHANNEL=2',
+                1 => 'ENV_TEST_CHANNEL_READABLE=test_2',
+                2 => 'ENV_TEST_CHANNELS_NUMBER=10',
+                3 => 'ENV_TEST_ARGUMENT=fileA',
+                4 => 'ENV_TEST_INC_NUMBER=10',
+                5 => 'ENV_TEST_IS_FIRST_ON_CHANNEL=1'
+            ),
+            $process->getenv()
+        );
     }
 
     /**
@@ -34,14 +37,17 @@ class ProcessFactoryTest extends \PHPUnit_Framework_TestCase
         $process = $factory->createAProcess('fileA', 2, 12, false);
 
         $this->assertEquals('execute', $process->getCommandLine());
-        $this->assertEquals(array(
+        $this->assertEquals(
+            $_ENV + array(
                 0 => 'ENV_TEST_CHANNEL=2',
                 1 => 'ENV_TEST_CHANNEL_READABLE=test_2',
                 2 => 'ENV_TEST_CHANNELS_NUMBER=11',
                 3 => 'ENV_TEST_ARGUMENT=fileA',
                 4 => 'ENV_TEST_INC_NUMBER=12',
-                5 => 'ENV_TEST_IS_FIRST_ON_CHANNEL=0'),
-            $process->getenv());
+                5 => 'ENV_TEST_IS_FIRST_ON_CHANNEL=0'
+            ),
+            $process->getenv()
+        );
     }
 
     /**
@@ -53,14 +59,17 @@ class ProcessFactoryTest extends \PHPUnit_Framework_TestCase
         $process = $factory->createAProcess('fileA', 1, 13, true);
 
         $this->assertEquals('execute 1 fileA', $process->getCommandLine());
-        $this->assertEquals(array(
+        $this->assertEquals(
+            $_ENV + array(
                 0 => 'ENV_TEST_CHANNEL=1',
                 1 => 'ENV_TEST_CHANNEL_READABLE=test_1',
                 2 => 'ENV_TEST_CHANNELS_NUMBER=12',
                 3 => 'ENV_TEST_ARGUMENT=fileA',
                 4 => 'ENV_TEST_INC_NUMBER=13',
-                5 => 'ENV_TEST_IS_FIRST_ON_CHANNEL=1'),
-            $process->getenv());
+                5 => 'ENV_TEST_IS_FIRST_ON_CHANNEL=1'
+            ),
+            $process->getenv()
+        );
     }
 }
  
