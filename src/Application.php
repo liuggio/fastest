3f0be18a647364f41f6c6be989b2d55e721046f1
Alexis Lefebvre <alexislefebvre@users.noreply.github.com>
2016-07-19 13:44:19 +0200

    Update version in Application.php

--- src/Application.php
+++ src/Application.php
@@ -7,7 +7,7 @@ use Symfony\Component\Console\Input\InputInterface;
 
 class Application extends BaseApp
 {
-    const VERSION = '1.2.1';
+    const VERSION = '1.4';
 
     public function __construct()
     {
