8ed97a4d4b50bd5a4a891c3ff47f0a1305c778b2
tarlepp <tarmo.leppanen@protacon.com>
2017-12-06 20:46:15 +0200

    Fixed env variable usages

--- src/Process/Processes.php
+++ src/Process/Processes.php
@@ -2,6 +2,7 @@
 
 namespace Liuggio\Fastest\Process;
 
+use Liuggio\Fastest\Queue\TestSuite;
 use Symfony\Component\Process\Process;
 
 class Processes
@@ -15,7 +16,7 @@ class Processes
      * @var float[] Unix timestamp with float part of processes start times
      */
     private $startTimes;
-    
+
     /**
      * @var Report[]
      */
@@ -185,12 +186,12 @@ class Processes
     }
 
     /**
-     * @throws LogicException
+     * @throws \LogicException
      */
     private function assertTerminated($key)
     {
         if (!$this->processes[$key]->isTerminated()) {
-            throw new LogicException('Process must be terminated before calling');
+            throw new \LogicException('Process must be terminated before calling');
         }
     }
 
@@ -201,9 +202,9 @@ class Processes
     private function moveToCompletedProcesses($key, Process $process)
     {
         $env = $process->getEnv();
-        $suite = str_replace(EnvCommandCreator::ENV_TEST_ARGUMENT.'=', '', $env[3]);
-        $number = str_replace(EnvCommandCreator::ENV_TEST_CHANNEL.'=', '', $env[0]);
-        $numberOnThread = (int) str_replace(EnvCommandCreator::ENV_TEST_IS_FIRST_ON_CHANNEL.'=', '', $env[5]);
+        $suite = $env['ENV_TEST_ARGUMENT'] instanceof TestSuite ? $env['ENV_TEST_ARGUMENT']->getTestPath() : $env['ENV_TEST_ARGUMENT'];
+        $number = $env['ENV_TEST_CHANNEL'];
+        $numberOnThread = $env['ENV_TEST_IS_FIRST_ON_CHANNEL'];
 
         if (!$process->isSuccessful()) {
             $this->errorCounter++;
