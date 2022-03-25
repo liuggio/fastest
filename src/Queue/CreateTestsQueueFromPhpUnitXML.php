5f16a0d586331fdd09f74f9945a41540ab1c7377
DonCallisto <samuele.lilli@gmail.com>
2022-03-25 11:49:16 +0100

    Fixated PHPStan version

--- src/Queue/CreateTestsQueueFromPhpUnitXML.php
+++ src/Queue/CreateTestsQueueFromPhpUnitXML.php
@@ -6,37 +6,78 @@ class CreateTestsQueueFromPhpUnitXML
 {
     public static function execute(string $xmlFile): TestsQueue
     {
-        // phpunit 9.0 compatibility
-        if (class_exists('\PHPUnit\TextUI\Configuration\Configuration')) {
-            $configuration = \PHPUnit\TextUI\Configuration\Registry::getInstance()->get($xmlFile);
-
+        // phpunit 8.5 compatibility
+        if (class_exists('\PHPUnit\Util\Configuration')) {
+            $configuration = \PHPUnit\Util\Configuration::getInstance($xmlFile);
             $testSuites = new TestsQueue();
 
-            self::handlePhpUnitBootstrapV90($configuration->phpunit());
-            self::processTestSuiteCollectionV90($testSuites, $configuration->testSuite());
+            $config = $configuration->getPHPUnitConfiguration();
+            $filename = isset($config['bootstrap']) ? $config['bootstrap'] : 'vendor/autoload.php';
+
+            \PHPUnit\Util\FileLoader::checkAndLoad($filename);
+
+            self::processTestSuites($testSuites, $configuration->getTestSuiteConfiguration()->getIterator());
+
+            return $testSuites;
+        }
+
+        // phpunit < 9.3 compatibility
+        if (class_exists('\PHPUnit\TextUI\Configuration\Registry') && class_exists('\PHPUnit\TextUI\Configuration\TestSuiteMapper')) {
+            $configuration = \PHPUnit\TextUI\Configuration\Registry::getInstance()->get($xmlFile);
+            $testSuites = new TestsQueue();
+
+            $PHPUnit = $configuration->phpunit();
+            $filename = $PHPUnit->hasBootstrap() ? $PHPUnit->bootstrap() : 'vendor/autoload.php';
+
+            \PHPUnit\Util\FileLoader::checkAndLoad($filename);
+
+            $testSuite = (new \PHPUnit\TextUI\Configuration\TestSuiteMapper)->map($configuration->testSuite(), '');
+            self::processTestSuites($testSuites, $testSuite->getIterator());
 
             return $testSuites;
         }
 
         // phpunit 9.3 compatibility
-        if (class_exists('\PHPUnit\TextUI\XmlConfiguration\Configuration')) {
+        if (class_exists('\PHPUnit\TextUI\XmlConfiguration\Loader') && class_exists('\PHPUnit\TextUI\XmlConfiguration\TestSuiteMapper')) {
             $configuration = (new \PHPUnit\TextUI\XmlConfiguration\Loader)->load($xmlFile);
 
             $testSuites = new TestsQueue();
 
-            self::handlePhpUnitBootstrapV93($configuration->phpunit());
-            self::processTestSuiteCollectionV93($testSuites, $configuration->testSuite());
+            $PHPUnit = $configuration->phpunit();
+            $filename = $PHPUnit->hasBootstrap() ? $PHPUnit->bootstrap() : 'vendor/autoload.php';
+
+            \PHPUnit\Util\FileLoader::checkAndLoad($filename);
+            $testSuiteCollection = $configuration->testSuite();
+
+            $testSuite = (new \PHPUnit\TextUI\XmlConfiguration\TestSuiteMapper)->map($testSuiteCollection, '');
+            $iterator = $testSuite->getIterator();
+
+            self::processTestSuites($testSuites, $iterator);
 
             return $testSuites;
         }
 
-        $configuration = \PHPUnit\Util\Configuration::getInstance($xmlFile);
-        $testSuites = new TestsQueue();
+        // phpunit > 9.3 compatibility
+        if (class_exists('\PHPUnit\TextUI\XmlConfiguration\Loader') && class_exists('\PHPUnit\TextUI\TestSuiteMapper')) {
+            $configuration = (new \PHPUnit\TextUI\XmlConfiguration\Loader)->load($xmlFile);
 
-        self::handleBootstrap($configuration->getPHPUnitConfiguration());
-        self::processTestSuite($testSuites, $configuration->getTestSuiteConfiguration()->getIterator());
+            $testSuites = new TestsQueue();
 
-        return $testSuites;
+            $PHPUnit = $configuration->phpunit();
+            $filename = $PHPUnit->hasBootstrap() ? $PHPUnit->bootstrap() : 'vendor/autoload.php';
+
+            \PHPUnit\Util\FileLoader::checkAndLoad($filename);
+            $testSuiteCollection = $configuration->testSuite();
+
+            $testSuite = (new \PHPUnit\TextUI\TestSuiteMapper)->map($testSuiteCollection, '');
+            $iterator = $testSuite->getIterator();
+
+            self::processTestSuites($testSuites, $iterator);
+
+            return $testSuites;
+        }
+
+        return new TestsQueue();
     }
 
     /**
@@ -45,7 +86,7 @@ class CreateTestsQueueFromPhpUnitXML
      *
      * @throws \ReflectionException
      */
-    private static function processTestSuite(
+    private static function processTestSuites(
         TestsQueue $testSuites,
         \Iterator $testSuiteIterator
     ): void {
@@ -53,30 +94,9 @@ class CreateTestsQueueFromPhpUnitXML
             self::addTestFile($testSuites, $testSuite);
 
             if ($testSuite instanceof \PHPUnit\Framework\TestSuite) {
-                self::processTestSuite($testSuites, $testSuite->getIterator());
+                self::processTestSuites($testSuites, $testSuite->getIterator());
             }
         }
-    }
-
-    private static function processTestSuiteCollectionV90(
-        TestsQueue $testSuites,
-        \PHPUnit\TextUI\Configuration\TestSuiteCollection $testSuiteCollection
-    ): void {
-        $testSuite = (new \PHPUnit\TextUI\Configuration\TestSuiteMapper)->map($testSuiteCollection, '');
-        self::processTestSuite($testSuites, $testSuite->getIterator());
-    }
-
-    private static function processTestSuiteCollectionV93(
-        TestsQueue $testSuites,
-        \PHPUnit\TextUI\XmlConfiguration\TestSuiteCollection $testSuiteCollection
-    ): void {
-        // phpunit 9.5 compatibility
-        if (class_exists('\PHPUnit\TextUI\TestSuiteMapper')) {
-            $testSuite = (new \PHPUnit\TextUI\TestSuiteMapper)->map($testSuiteCollection, '');
-        } else {
-            $testSuite = (new \PHPUnit\TextUI\XmlConfiguration\TestSuiteMapper)->map($testSuiteCollection, '');
-        }
-        self::processTestSuite($testSuites, $testSuite->getIterator());
     }
 
     /**
@@ -96,42 +116,5 @@ class CreateTestsQueueFromPhpUnitXML
 
             $testSuites->add($fileName);
         }
-    }
-
-    /**
-     * @param array<string, bool|string|int> $config
-     *
-     * @return void
-     */
-    private static function handleBootstrap(array $config): void
-    {
-        /** @var string $filename */
-        $filename = isset($config['bootstrap']) ? $config['bootstrap'] : 'vendor/autoload.php';
-
-        \PHPUnit\Util\FileLoader::checkAndLoad($filename);
-    }
-
-    /**
-     * Loads a bootstrap file.
-     *
-     * @param \PHPUnit\TextUI\Configuration\PHPUnit $PHPUnit The Phpunit config
-     */
-    private static function handlePhpUnitBootstrapV90(\PHPUnit\TextUI\Configuration\PHPUnit $PHPUnit): void
-    {
-        $filename = $PHPUnit->hasBootstrap() ? $PHPUnit->bootstrap() : 'vendor/autoload.php';
-
-        \PHPUnit\Util\FileLoader::checkAndLoad($filename);
-    }
-
-    /**
-     * Loads a bootstrap file.
-     *
-     * @param \PHPUnit\TextUI\XmlConfiguration\PHPUnit $PHPUnit The Phpunit config
-     */
-    private static function handlePhpUnitBootstrapV93(\PHPUnit\TextUI\XmlConfiguration\PHPUnit $PHPUnit): void
-    {
-        $filename = $PHPUnit->hasBootstrap() ? $PHPUnit->bootstrap() : 'vendor/autoload.php';
-
-        \PHPUnit\Util\FileLoader::checkAndLoad($filename);
     }
 }
